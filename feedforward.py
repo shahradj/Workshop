a5b7840dbaadbd076e7e6e78436005a59d6f6192
Shahrad <shahradj@gmail.com>
2016-12-01 11:58:10 -0800

    the code for the workshop

--- feedforward.py
+++ feedforward.py
@@ -1,39 +1,83 @@
 import lasagne
 from lasagne import layers
 
+from sklearn.datasets import load_iris
 import theano
 import theano.tensor as T
 import os
 import numpy as np
+import random
 
 class NN():
 	"""
 		The reinforcement learning language neural network
 	"""
-	def __init__(self, n_in=100, n_hidden=500, n_out=5):
+	def __init__(self, n_in=4, n_hidden=50, n_out=3,classification=True):
 		self.hidden=n_hidden
-		self.makeNetwork(n_in,n_hidden,n_out)
+		if classification:
+			self.makeClassificationNetwork(n_in,n_hidden,n_out)
+		else:
+			self.makeRegressionNetwork(n_in,n_hidden,n_out)
 
-	def makeNetwork(self,n_in,n_hidden,n_out,learning_rate=0.00002):
-		x = T.tensor3s('x')  # shp: num_batch x num_features
-		y = T.ivector('y')# shp: num_batch
-		l_input=layers.InputLayer((30,None,n_in))
-		# bs,_=x.shape
-		# l_rinput=layers.ReshapeLayer(l_input,((bs,None,100)))
-		l_forward_1 = lasagne.layers.LSTMLayer(l_input, 500, nonlinearity=lasagne.nonlinearities.tanh)
-		l_forward_slice = lasagne.layers.SliceLayer(l_forward_1, -1, 1)
-		l_hid=layers.DenseLayer(l_forward_slice,num_units=n_hidden)
+	def makeClassificationNetwork(self,n_in,n_hidden,n_out,learning_rate=0.001):
+		"""
+			build a feedforward neural network with classification output
+		"""
+		#network input
+		input_ = T.matrix('input_')  # matrix of shape batch size times number of input variables
+		target_ = T.ivector('target_')# integer vector of batchsize length
+
+		#network
+		l_input=layers.InputLayer((None,n_in))
+		l_hid=layers.DenseLayer(l_input,num_units=n_hidden)
 		self.l_out=layers.DenseLayer(l_hid,num_units=n_out,nonlinearity=lasagne.nonlinearities.softmax)
-		
-		l_outvalue = layers.get_output(self.l_out, x)
-		params = layers.get_all_params(self.l_out)
-		loss = T.mean(lasagne.objectives.categorical_crossentropy(l_outvalue, y))
-		updates = lasagne.updates.nesterov_momentum(loss, params, learning_rate=learning_rate, momentum=0.9)
-		self.train_function = theano.function([x, y],loss, updates=updates,allow_input_downcast=True)
-		self.pred_function=theano.function([x],l_outvalue,allow_input_downcast=True)
 
-	def testNetwork(self,train,target):
-		predictions=self.pred_function(train)
+		#network output
+		l_outvalue = layers.get_output(self.l_out, input_)
+		self.predict=theano.function([input_],l_outvalue,allow_input_downcast=True)
+
+		#loss/cost function
+		loss = T.mean(lasagne.objectives.categorical_crossentropy(l_outvalue, target_))
+
+		#calculate the updates
+		params = layers.get_all_params(self.l_out)
+		updates = lasagne.updates.nesterov_momentum(loss, params, learning_rate=learning_rate, momentum=0.9)
+	
+		#update the weights from a given input and target
+		self.train_function = theano.function([input_, target_],loss, updates=updates,allow_input_downcast=True)
+
+	def makeRegressionNetwork(self,n_in,n_hidden,n_out,learning_rate=0.001):
+		"""
+			build a feedforward neural network with regression output
+		"""
+		#network input
+		input_ = T.matrix('input_')  # matrix of shape batch size times number of input variables
+		target_ = T.matrix('target_')# matrix of shape batch size times number of output variables
+		
+		#network
+		l_input=layers.InputLayer((None,n_in))
+		l_hid=layers.DenseLayer(l_input,num_units=n_hidden)
+		self.l_out=layers.DenseLayer(l_hid,num_units=n_out,nonlinearity=None)
+			
+		#network output
+		l_outvalue = layers.get_output(self.l_out, input_)
+		self.predict=theano.function([input_],l_outvalue,allow_input_downcast=True)
+		
+		#loss/cost function
+		loss = T.mean(lasagne.objectives.squared_error(l_outvalue, target_))
+		
+		#calculate the updates
+		params = layers.get_all_params(self.l_out)
+		updates = lasagne.updates.nesterov_momentum(loss, params, learning_rate=learning_rate, momentum=0.9)
+
+		#update the weights from a given input and target
+		self.train_function = theano.function([input_, target_],loss, updates=updates,allow_input_downcast=True)
+
+	def testNetwork(self,input_,target):
+		"""
+			returns percentage accuracy of the network on a set of known targets (only for classification networks)
+		"""
+		predictions=self.predict(input_).argmax(axis=1)
 		correctPredictions=[p==t for p,t in zip(predictions,target)]
 		percentage_accuracy=sum(correctPredictions)*1.0/len(target)
 		return percentage_accuracy
@@ -52,3 +96,30 @@ class NN():
 		thefile=open(filename,'r')
 		oldvals=cPickle.load(thefile)
 		layers.set_all_param_values(self.l_out,oldvals)
+
+if __name__=='__main__':
+	net=NN()
+	iris=load_iris()
+	#separate the data into train and test samples
+	trainIndices=random.sample(range(150),125)
+	testIndices=set(range(150)).difference(trainIndices)
+	traininput_=[iris.data[k] for k in trainIndices]
+	traintarget_=[iris.target[k] for k in trainIndices]
+	testinput_=[iris.data[k] for k in testIndices]
+	testtarget_=[iris.target[k] for k in testIndices]
+	
+	batchsize=5
+	n_epochs=10
+	totalcosts=[]
+	for epoch in range(n_epochs):
+		#train the network
+		for j in range(len(traininput_)/batchsize):
+			input_=traininput_[j*batchsize:(j+1)*batchsize]
+			target_=traintarget_[j*batchsize:(j+1)*batchsize]
+			cost=net.train_function(input_,target_)
+			totalcosts.append(cost)
+			print 'Mean cost:',np.mean(totalcosts)
+		
+		#test the network
+		perc_acc=net.testNetwork(testinput_,testtarget_)
+		print 'Percentage accuracy of test set:%f'%perc_acc
